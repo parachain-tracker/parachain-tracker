697f15f7e91b7c37ff2f93e3e8bf92f023d9c680
hskang9 <hskang9@gmail.com>
2019-08-06 13:05:36 +0900

    Add view button and svgs

--- apps/parachain-tracker/src/app/home/hero/hero.component.ts
+++ apps/parachain-tracker/src/app/home/hero/hero.component.ts
@@ -3,17 +3,27 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 @Component({
   selector: 'pt-hero',
   template: `      
-    <div>
-        <div class="hero-text">
-            <h1>Explore DApps and Parachains on the Polkadot Network</h1>
-            <h2>Discover the Pinnacle of blockchain technology in parachains and possibility with DApps</h2>
-            <a href="" class="hero-link">Learn more about DApps</a>
-            <br>
-            <a href="" class="hero-link">Learn more about Parachains</a>
+    <div class="hero-wrapper">
+        <div class="hero-content">
+            <div class="hero-text">
+                <h1>Explore DApps and Parachains on the Polkadot Network</h1>
+                <h2>Discover the Pinnacle of blockchain technology in parachains and possibility with DApps</h2>
+                <div class="goto-container">
+                    <div class="goto"></div>
+                    <a href="" class="hero-link">Learn more about DApps</a>
+                </div>
+                <div class="goto-container">
+                    <div class="goto"></div>
+                    <a href="" class="hero-link">Learn more about Parachains</a>
+                </div>
+                <button class="goto-button"><p>View Dapps & Parachains</p></button>
+            </div>
+            <div class="hero-svg"></div>
         </div>
-        <div class="hero-svg"></div>
+        
     </div>
     <section></section>
+    
   `,
   styleUrls: ['./hero.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
