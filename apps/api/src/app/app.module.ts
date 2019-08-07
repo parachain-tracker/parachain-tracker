d6c1da0234226899a27544893ed814d0096c5000
stupidawesome <stupidawesome@zerocents.com.au>
2019-08-07 21:23:33 +0900

    feat: setup sqlite database, typeorm

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from "@nestjs/common"
-
 import { AppController } from "./app.controller"
 import { AppService } from "./app.service"
+import { DappsModule } from "./dapps/dapps.module"
+import { TypeOrmModule } from "@nestjs/typeorm"
+import { ConnectionOptions } from "typeorm"
+import { DappEntity } from "./database/entity/dapp.entity"
+import { CategoryEntity } from "./database/entity/category.entity"
+import { ExternalLinkEntity } from "./database/entity/external-link.entity"
+
+const ormconfig = require("../../../../ormconfig.json")
+
+const connectionOptions: ConnectionOptions = {
+    ...ormconfig,
+    entities: [DappEntity, CategoryEntity, ExternalLinkEntity],
+}
 
 @Module({
-    imports: [],
+    imports: [TypeOrmModule.forRoot(connectionOptions), DappsModule],
     controllers: [AppController],
     providers: [AppService],
 })
