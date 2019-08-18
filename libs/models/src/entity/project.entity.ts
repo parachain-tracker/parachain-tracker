35503372bfde1c82f01dee4890b8a0e4d57638c5
kalium <kalium@kalium.xyz>
2019-08-18 22:32:34 +0900

    fix: project to category must be many to one

--- libs/models/src/entity/project.entity.ts
+++ libs/models/src/entity/project.entity.ts
@@ -4,7 +4,7 @@ import {
     JoinColumn,
     JoinTable,
     ManyToMany,
-    OneToOne,
+    ManyToOne,
     PrimaryGeneratedColumn,
 } from "typeorm"
 import { CategoryEntity } from "./category.entity"
@@ -37,10 +37,10 @@ export class ProjectEntity {
     @Column("text")
     public link: string
 
-    @Column("int")
+    @Column("int", { default: 0 })
     public stars: number
 
-    @Column("int")
+    @Column("int", { default: 0 })
     public commits: number
 
     @Column({ length: 32 })
@@ -54,7 +54,7 @@ export class ProjectEntity {
     public externalLinks
 
     @JoinColumn()
-    @OneToOne(type => CategoryEntity)
+    @ManyToOne(type => CategoryEntity)
     public category: CategoryEntity
 
     @Column("text")
